5ec784ca1c1e07c13ab80d036c0a15d625a2d30e
palla023 <65664262+palla023@users.noreply.github.com>
2024-10-23 20:47:41 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 
