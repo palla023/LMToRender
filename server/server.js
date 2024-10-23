c3aa85cefa8d402d2aeb8b7a1a54b5fe3fb63230
palla023 <65664262+palla023@users.noreply.github.com>
2024-10-23 20:37:18 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
-app.use(express.json());
-require("dotenv").config();
 const port = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+
+dotenv.config();
+app.use(express.json());
+app.use(cors({ origin: "*" }));
 
 //mongodb config
 mongoose
